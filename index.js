b66eff72b08b328379302f982cb6c4fdc8f51a88
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2022-08-06 20:21:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+
+function fn1()
+{
+    var sub1= document.getElementById("subject1").value;
+    var sub2= document.getElementById("subject2").value;
+    var sub3= document.getElementById("subject3").value;
+    var sub4= document.getElementById("subject4").value;
+    var sub5= document.getElementById("subject5").value;
+     
+    var total=(+sub1)+(+sub2)+(+sub3)+(+sub4)+(+sub5)
+    var finaltotal=total/500*100
+
+    alert("Your total percentage is: "+finaltotal);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
